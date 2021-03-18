3232aec5c9e5e0d9f56c502ae6610a8c421623ed
Marina <59792949+Layrovell@users.noreply.github.com>
2021-03-18 11:33:32 +0200

    Update addForm.js

--- src/scripts/classes/addForm.js
+++ src/scripts/classes/addForm.js
@@ -1,59 +1,3 @@
-// import {Controllers} from "./controllers.js";
-//
-// export class AddForm {
-//   constructor() {
-//     this.newContact = [];
-//     this.name = '';
-//     this.number = '';
-//   }
-//
-//   createAddForm() {
-//     const root = document.getElementById('root')
-//     const form = document.createElement('form');
-//     form.setAttribute('class', 'form');
-//     const button = document.createElement("button");
-//     button.innerHTML = "Add";
-//     button.type = 'submit';
-//     button.setAttribute('class', 'form-button');
-//
-//     ['name', 'number'].forEach(labelName => {
-//         const isName = labelName === 'name';
-//         const label = document.createElement('label');
-//         const input = document.createElement('input');
-//         input.type = 'text';
-//         input.id = `form-${labelName}`;
-//         input.placeholder = isName ? 'Alyona' : '+8 880 2020 534';
-//
-//         input.addEventListener('change', (e) => {
-//           if (e.target.value) {
-//             this[labelName] = e.target.value;
-//           }
-//         });
-//
-//         form.appendChild(label);
-//         label.htmlFor = input.id;
-//         label.innerHTML = isName ? 'First name:' : 'Phone:';
-//         label.after(input);
-//     });
-//
-//     form.appendChild(button);
-//
-//     form.addEventListener('submit', async (e) => {
-//       e.preventDefault();
-//       // await this.addNewContact(this.number, this.name);
-//       await new Controllers().addItem(this.name, this.number, this.newContact);
-//       // console.log(this.newContact);
-//       await new Controllers().addItem(this.name, this.number, this.newContact);
-//     });
-//     root.before(form);
-//   }
-// }
-//
-// const createForm = new AddForm();
-// const addFormToPage = createForm.createAddForm();
-
-// import {Controllers} from "./controllers.js";
-
 export class AddForm {
   constructor(config) {
     this.newContact = [];
@@ -106,5 +50,3 @@ export class AddForm {
     console.log(this)
   }
 }
-
-// const qqq = new AddForm().createAddForm();
